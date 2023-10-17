29b6b8c0e037e19447fbad7112e30712d20f2237
Robert Green <robert.green.it.2017@gmail.com>
2023-10-17 05:21:44 -0400

    fix issue w/company type

--- src/app/models/dto/interfaces/ICompanyInformationDto.ts
+++ src/app/models/dto/interfaces/ICompanyInformationDto.ts
@@ -1,12 +1,15 @@
-﻿ export interface ICompanyInformationDto {
-    Id: number;
-    Name: string;
-    Website: string;
-    Email: string;
-    Phone: string;
-    StreetAddress1: string;
-    StreetAddress2: string;
-    City: string;
-    State: string;
-    Zip: string;
+import { ICompanyTypeDto } from "./ICompanyTypeDto";
+
+export interface ICompanyInformationDto {
+  Id: number;
+  Name: string;
+  Website: string;
+  Email: string;
+  Phone: string;
+  StreetAddress1: string;
+  StreetAddress2: string;
+  City: string;
+  State: string;
+  Zip: string;
+  Type: ICompanyTypeDto;
 }
