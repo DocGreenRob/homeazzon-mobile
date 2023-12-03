62776f8207593af3e0511e85af783ffaaf58309e
Robert Green <robert.green.it.2017@gmail.com>
2023-12-02 22:03:48 -0500

    gradle change

--- src/app/pages/features/account/sign-in/sign-in.page.ts
+++ src/app/pages/features/account/sign-in/sign-in.page.ts
@@ -124,7 +124,8 @@ export class SignInPage extends BasePage {
 
   async signInAzure() {
     // TODO: pull from config (.env file)
-    const url = `https://cognitivegenerationenterpr.b2clogin.com/cognitivegenerationenterpr.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpSignIn_Public_HomeaZZon&client_id=236c9456-da32-4c2c-81b4-842dfd0442f1&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A8100%2Fredirect&scope=openid&response_type=id_token&prompt=login`;
+    //const url = `https://cognitivegenerationenterpr.b2clogin.com/cognitivegenerationenterpr.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpSignIn_Public_HomeaZZon&client_id=236c9456-da32-4c2c-81b4-842dfd0442f1&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A8100%2Fredirect&scope=openid&response_type=id_token&prompt=login`;
+    const url = `https://cognitivegenerationenterpr.b2clogin.com/cognitivegenerationenterpr.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpSignIn_Public_HomeaZZon&client_id=236c9456-da32-4c2c-81b4-842dfd0442f1&nonce=defaultNonce&redirect_uri=https%3A%2F%2FhomeaZZon%2Ecom%2Fredirect&scope=openid&response_type=id_token&prompt=login`;
     const browser = this.iab.create(url, '_self', {
       location: 'no',
       clearcache: 'yes',
