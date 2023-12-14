fd232f014e8de22a7262ab25fe53674120ec46be
Hamza <Hamza01541@gmail.com>
2023-12-14 14:04:02 +0500

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,7 +66,6 @@ export class AppComponent extends BasePage {
   userProperties: any = [];
   getPropertiesSubsription: any;
   constants = new Constants();
-  userInfo:any;
 
   //new
   constructor(
