45c3f60f3bb8b86998ca8a67c9c32e44e6208c02
Yasir <yasir@yuzee.com>
2023-12-30 17:15:37 +0500

    2169: List and Grid View

---
+++ src/app/components/icon-list-grid/icon-list-grid.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IGrid } from 'src/app/models/dto/interfaces/IGrid';
+import { IGridListItem } from 'src/app/models/dto/interfaces/IGridListItem';
+import { IGridList } from 'src/app/models/dto/interfaces/IGridList';
+
+@Component({
+	selector: 'icon-list-grid',
+	templateUrl: './icon-list-grid.component.html',
+	styleUrls: ['./icon-list-grid.component.scss'],
+})
+export class IconListGridComponent implements OnInit {
+	showAllItems: boolean = false;
+	@Input() _grid: IGrid | any = {} as IGrid;
+	@Output() _itemClickEventHandler: any = new EventEmitter();
+
+	dafaultImage = "https://firebasestorage.googleapis.com/v0/b/itt-content.appspot.com/o/Common%2Fassets%2Fsvgs%2Fsolid%2Fcamera.svg?alt=media&token=e0af850d-247e-41a0-84ff-e6faa5e815b6";
+
+	constructor() {
+		console.log('Hello IconListGridComponent Component');
+	}
+
+	ngOnInit() {
+	}
+
+	public emit(item: IGridListItem, listName: string) {
+		let gridList: IGridList = { Items: [item], Name: listName };
+		this._itemClickEventHandler.emit(gridList);
+  }
+
+  public toggleSeeAllItemsInList(c: any) {
+    c.showAllItems = !c.showAllItems;
+  }
+
+}
