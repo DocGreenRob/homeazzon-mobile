ff8949d2ff7933be4754fdd5facc32f3cd300cfc
Farrukh Qasim <133210350+FarukhQasim@users.noreply.github.com>
2024-03-08 18:55:46 +0500

    pdf viewer design completed

--- src/app/pages/features/private-label/single-design-plan/single-design-plan.page.ts
+++ src/app/pages/features/private-label/single-design-plan/single-design-plan.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+
 
 @Component({
 	selector: 'app-single-design-plan',
@@ -9,7 +9,6 @@ import { NavController } from '@ionic/angular';
 	styleUrls: ['./single-design-plan.page.scss'],
 })
 export class SingleDesignPlanPage implements OnInit {
-
 	title: string;
 	pdfUrl: string;
 	zoomCount: number = 1;
@@ -17,15 +16,14 @@ export class SingleDesignPlanPage implements OnInit {
 	@ViewChild('zoom', { static: false }) zoom: ElementRef;
 	constructor(public navCtrl: NavController, private activeRoute: ActivatedRoute) {
 	}
-
-
 	ngOnInit() {
 		console.log('ionViewDidLoad SingleDesignPlanPage');
 		this.activeRoute.queryParams.subscribe(params => {
-			this.title = params["title"];
-      this.pdfUrl = params["pdfUrl"];
+	// 		this.title = params["title"];
+    //   this.pdfUrl = params["pdfUrl"];
+	     this.title = "pdf demo";
+	     this.pdfUrl="https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 		})
-
 	}
 
 	close() {
@@ -33,18 +31,14 @@ export class SingleDesignPlanPage implements OnInit {
 	}
 
 	zoomReduce() {
-		this.zoomCount = this.zoomCount - 1;
+		this.zoomCount = this.zoomCount - 0.2;
 	}
 
 	zoomIncrease() {
-		this.zoomCount = this.zoomCount + 1;
+		this.zoomCount = this.zoomCount + 0.2;
 	}
 
 	zoomReset() {
 		this.zoomCount = 1;
 	}
-
-
-
-
 }
