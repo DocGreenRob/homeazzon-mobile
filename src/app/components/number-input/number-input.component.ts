c9eab7776d66e6c56e0a2035769d652d5e68cbac
Yasir <yasir@yuzee.com>
2024-03-16 23:33:39 +0500

    1. 2871 2. 2897 3. 2896 4. 2903 5. 2901 6. 2902 7. Change Loader Font match figma

--- src/app/components/number-input/number-input.component.ts
+++ src/app/components/number-input/number-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-number-input',
@@ -9,12 +9,28 @@ export class NumberInputComponent implements OnInit {
   @Input() placeholder: string = '';
   @Input() value: number = 0;
   @Output() valueChange: EventEmitter<any> = new EventEmitter();
+  @ViewChild('inputField') inputField!: ElementRef<HTMLInputElement>;
 
   constructor() { }
 
   ngOnInit() {
     if (!this.value) {
       this.value = 0;
+    }
+  }
+
+  handleInputClick() {
+    if (this.value === 0) {
+      this.value = null;
+    }
+    if (this.inputField && this.inputField.nativeElement) {
+      this.inputField.nativeElement.focus();
+    }
+  }
+  handleInputOut() {
+    if (!this.value && this.value !== 0) {
+      this.value = 0;
+      this.valueChange.emit(this.value);
     }
   }
 
