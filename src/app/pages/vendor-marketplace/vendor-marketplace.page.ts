aa16acbeaee328ef4b8cb1cc67c4331561978ed7
Hamza <Hamza01541@gmail.com>
2024-03-18 17:54:35 +0500

    Created market place vendor page design

--- src/app/pages/vendor-marketplace/vendor-marketplace.page.ts
+++ src/app/pages/vendor-marketplace/vendor-marketplace.page.ts
@@ -1,13 +1,4 @@
 import { Component } from '@angular/core';
-import { BasePage } from '../base/base.page';
-import { Router } from '@angular/router';
-import { CommunicatorService } from '@app/services/communicator/communicator.service';
-import { FeaturesService } from '@app/services/features/features.service';
-import { LocalStorageService } from '@app/services/local-storage.service';
-import { UserTypesService } from '@app/services/user-types/user-types.service';
-import { UxNotifierService } from '@app/services/uxNotifier/ux-notifier.service';
-import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
-import { NavController, AlertController, LoadingController, MenuController, Platform } from '@ionic/angular';
 import { register } from 'swiper/element/bundle';
 
 register();
@@ -16,39 +7,8 @@ register();
   templateUrl: './vendor-marketplace.page.html',
   styleUrls: ['./vendor-marketplace.page.scss'],
 })
-export class VendorMarketplacePage extends BasePage {
-  constructor(
-    public override navController: NavController,
-    public alertCtrl: AlertController,
-    public loadingCtrl: LoadingController,
-    public override communicator: CommunicatorService,
-    public override menuController: MenuController,
-    public override router: Router,
-    public override userTypesService: UserTypesService,
-    public featureService: FeaturesService,
-    public override uxNotifierService: UxNotifierService,
-    public override platform: Platform,
-    public override inAppBrowser: InAppBrowser,
-    public override storageService: LocalStorageService
-    ) {
-      super(
-        navController,
-        null,
-        communicator,
-        menuController,
-        platform,
-        router,
-        uxNotifierService,
-        userTypesService,
-        featureService,
-        inAppBrowser,
-        storageService
-      );
-   }
+export class VendorMarketplacePage {
 
-   override async ngOnInit() {
-    console.log('ngOnInit vendor-marketplace');
-    console.log("this.user:", this.User)
-  }
+  constructor() {}
 
 }
